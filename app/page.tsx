37b84728df5d9305c73db0c9c4b78ed86215c403
benko789 <benkokaiser@hotmail.com>
2025-03-29 21:59:45 +1100

    Fix component size prevent leaking

--- app/page.tsx
+++ app/page.tsx
@@ -54,11 +54,11 @@ export default function Home() {
         </div>
 
         {/* Content Container */}
-        <div className="w-full max-w-3xl">
+        <div className="md:w-full md:max-w-3xl">
           <div className="flex flex-col md:flex-row items-center justify-between w-full">
-            <div className="text-center md:text-left space-y-0 w-full max-w-[26rem]">
-              <h1 className={`text-[11rem] md:text-[12rem] font-mono font-bold leading-[0.8] pl-1.5 ${isHolding ? 'text-gradient-mask active' : 'text-gradient-mask'}`}>BEN</h1>
-              <h1 ref={koRef} className={`text-[15.8rem] md:text-[17.25rem] font-mono font-bold leading-[0.8] ${isHolding ? 'text-gradient-mask active' : 'text-gradient-mask'}`}>KO</h1>
+            <div className="text-left md:text-left space-y-0 md:w-full max-w-[26rem]">
+              <h1 className={`text-[10.8rem] md:text-[12rem] font-mono font-bold leading-[0.8] pl-1.5 ${isHolding ? 'text-gradient-mask active' : 'text-gradient-mask'}`}>BEN</h1>
+              <h1 ref={koRef} className={`text-[15.6rem] md:text-[17.25rem] font-mono font-bold leading-[0.8] ${isHolding ? 'text-gradient-mask active' : 'text-gradient-mask'}`}>KO</h1>
               <GradientMaskButton isHolding={isHolding} onHoldChange={setIsHolding} className="absolute" style={
                 { 
                   top: buttonPosition.top, 
@@ -69,7 +69,7 @@ export default function Home() {
                 }
               } />
             </div>
-            <div className="text-left md:space-y-2 ml-7 md:ml-3 w-full max-w-[23.5rem]">
+            <div className="text-left md:space-y-2 pl-4 md:pl-3 w-full md:max-w-[23.5rem]">
               {/* Vertical List of Numbers and Descriptions */}
               {[
                 { number: 3, description: "Projects Completed" },
